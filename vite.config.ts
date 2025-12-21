269d93a844017560e247a3d677644e80275161f4
kevinccbsg <kevinccbsg@gmail.com>
2025-12-21 23:57:10 +0100

    fix: repo base

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: 'https://brikev.github.io/twd-shadcn/',
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/twd-shadcn/',
   plugins: [
     react({
       babel: {
@@ -19,4 +19,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
